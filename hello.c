97979af4077c21d6a7f9ce7a91d97f01448a8f73
Kingsley <kingsleyog95@gmail.com>
2020-07-24 21:08:14 +0100

    C prog to swap numbers

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void swap(int*a, int *b);
+int main ()
+{
+
+int first, second;
+printf("Enter two numbers: ");
+scanf("%d", &first);
+printf("Another: ");
+scanf("%d", &second);
+swap(&first, &second);
+printf("Your new numbers are %d and %d\n", first, second);
+
+}
+void swap (int* a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = temp;
+}
